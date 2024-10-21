cc8b04a61187167ddeeb2617fc92f04ee4ef29dc
Joice-Araujo <joice.aparecida21@hotmail.com>
2024-10-20 21:51:39 -0300

    :sparkles: graficopluvi ainda precisa de ajustes

--- frontend2/src/pages/dashboard/dashboard.tsx
+++ frontend2/src/pages/dashboard/dashboard.tsx
@@ -1,5 +1,5 @@
-import React, { } from "react";
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Button, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { CardDashbord } from "../../components/CardDashbord/cardDashbord";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Octicons from 'react-native-vector-icons/Octicons';
@@ -8,6 +8,9 @@ import { style } from "./styles";
 //import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RootStackParamList } from "../../navigation/types";
 import { RouteProp, useRoute } from '@react-navigation/native';
+import BarChartPluviometria from "../../components/ChartPluvi/chartPluvi";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { chartPluvi } from '../../@types/chartPluv';
 
 
 type DashboardRouteProp = RouteProp<RootStackParamList, 'Dashboard'>;
@@ -34,54 +37,128 @@ export default function Dashboard() {
     // const route = useRoute<DashboardRouteProp>();
     // const { temperatura, pluviometria, cultura } = route.params;
 
-
     const route = useRoute<DashboardRouteProp>();
     const { cultura } = route.params;
+
+
+    const [dataEscolhida, setDataEscolhida] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [chartData, setChartData] = useState<chartPluvi>({ labels: [], values: [] });
+
+    const onShowDatePicker = () => {
+        setShowDatePicker(true);
+    };
+
+    const dataInicial = new Date(cultura.pluviometrias[0]?.data);
+
+    const onDateChange = (event: any, dataEscolhida: React.SetStateAction<Date>) => {
+        setShowDatePicker(false);
+        if (dataEscolhida) {
+            setDataEscolhida(dataEscolhida);
+        }
+    };
+
+    useEffect(() => {
+        const filteredData = cultura.pluviometrias.filter(item => {
+            const itemDate = new Date(item.data);
+            return itemDate.getMonth() === dataEscolhida.getMonth() && itemDate.getFullYear() === dataEscolhida.getFullYear();
+        });
+
+
+        const semana = [0, 0, 0, 0];
+        filteredData.forEach(item => {
+            const day = new Date(item.data).getDate();
+            if (day >= 1 && day <= 7) semana[0] += item.pluviometria;
+            else if (day >= 8 && day <= 14) semana[1] += item.pluviometria;
+            else if (day >= 15 && day <= 21) semana[2] += item.pluviometria;
+            else if (day >= 22 && day <= 31) semana[3] += item.pluviometria;
+        });
+
+        const semanaFormatada = semana.map(value => parseFloat(value.toFixed(1)));
+
+        setChartData({
+            labels: ['1-7', '8-14', '15-21', '22-31'],
+            values: semanaFormatada,
+        });
+    }, [dataEscolhida, cultura]);
+
+
+
 
     return (
 
         <View style={style.container}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
 
-            <View style={style.titulo}>
-                <Text style={style.texto1}>{cultura.nome_cultivo}</Text>
-            </View>
-            <View style={style.cards}>
-                <CardDashbord
-                    title1="Máxima"
-                    valor={cultura.temperaturas.slice(-1)[0].temperatura_max.toString()}
-                    IconName="sun"
-                    Icon={Octicons}
-                    showTemperatura={true} />
+                <View style={style.titulo}>
+                    <Text style={style.texto1}>{cultura.nome_cultivo}</Text>
+                </View>
+                <View style={style.cards}>
+                    <CardDashbord
+                        title1="Máxima"
+                        valor={cultura.temperaturas.slice(-1)[0].temperatura_max.toString()}
+                        IconName="sun"
+                        Icon={Octicons}
+                        showTemperatura={true} />
 
-                <CardDashbord
-                    title1="Mínima"
-                    valor={cultura.temperaturas.slice(-1)[0].temperatura_min.toString()}
-                    IconName="partly-sunny-outline"
-                    Icon={Ionicons}
-                    showTemperatura={true}
-                />
+                    <CardDashbord
+                        title1="Mínima"
+                        valor={cultura.temperaturas.slice(-1)[0].temperatura_min.toString()}
+                        IconName="partly-sunny-outline"
+                        Icon={Ionicons}
+                        showTemperatura={true}
+                    />
 
-            </View>
+                </View>
 
-            <View style={style.cards}>
-                <CardDashbord
-                    title1="Temp. Média"
-                    valor={cultura.temperaturas.slice(-1)[0].temperatura_media.toString()}
-                    IconName="sun"
-                    Icon={Octicons}
-                    showTemperatura={true} />
+                <View style={style.cards}>
+                    <CardDashbord
+                        title1="Temp. Média"
+                        valor={cultura.temperaturas.slice(-1)[0].temperatura_media.toString()}
+                        IconName="sun"
+                        Icon={Octicons}
+                        showTemperatura={true} />
 
 
 
-                <CardDashbord
-                    title1="Chuva"
-                    valor={cultura.pluviometrias.slice(-1)[0].pluviometria.toString()}
-                    IconName="cloud-rain"
-                    Icon={FontAwesome6}
-                    showTemperatura={false}
-                />
+                    <CardDashbord
+                        title1="Chuva"
+                        valor={cultura.pluviometrias.slice(-1)[0].pluviometria.toString()}
+                        IconName="cloud-rain"
+                        Icon={FontAwesome6}
+                        showTemperatura={false}
+                    />
 
-            </View>
+                </View>
+
+                <View style={style.pluvi} >
+
+                    <View style={style.tituloPluvi}>
+                        <Text style={style.tituloTexto}>Pluviometria (mm)</Text>
+
+                        <TouchableOpacity style={style.btn} onPress={onShowDatePicker}>
+                            <Text style={style.buttonText}>Selecionar Mês</Text>
+                        </TouchableOpacity>
+
+                        {/* <Button title="Selecionar Mês" onPress={onShowDatePicker} /> */}
+
+                        {showDatePicker && (
+                            <DateTimePicker
+                                value={dataEscolhida}
+                                mode="date"
+                                display="default"
+                                onChange={onDateChange}
+                                minimumDate={dataInicial}
+                                maximumDate={new Date()} // Para impedir a seleção de datas futuras
+                            />
+                        )}
+                    </View>
+
+                    <BarChartPluviometria data={chartData} />
+
+                </View>
+
+            </ScrollView>
         </View>
 
     )
